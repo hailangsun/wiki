177840fe5e6f0abc0f79957958a5753dccea187c
Administrator <admin@example.com>
2021-10-27 23:21:42 +0800

    提交代码

---
+++ src/main/java/com/wiki/mapper/ContentMapper.java
@@ -0,0 +1,9 @@
+package com.wiki.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wiki.entity.Content;
+
+public interface ContentMapper extends BaseMapper<Content> {
+
+}
